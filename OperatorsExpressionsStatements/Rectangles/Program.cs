fee0f415e7f0e7dbe81d9fcd985fdc0c211b1b8d
nikydobrev <nikydobrev@gmail.com>
2014-03-16 21:05:57 +0200

    OperatorsExpressionsStatements

---
+++ OperatorsExpressionsStatements/Rectangles/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Rectangles
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Width = ");
+        double width = double.Parse(Console.ReadLine());
+        Console.Write("Height = ");
+        double height = double.Parse(Console.ReadLine());
+        Console.WriteLine("Area = {0};\nPerimeter = {1};", (width * height),(2*width + 2*height));
+    }
+}
+
